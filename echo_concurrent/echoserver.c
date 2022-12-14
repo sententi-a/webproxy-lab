83cf525e62c2315d961d0840034a62932c1aa24a
SeungHuLee <gadlsh12081208@gmail.com>
2022-12-14 11:19:31 +0900

    Add basic echo

--- echo_concurrent/echoserver.c
+++ echo_concurrent/echoserver.c
@@ -1,25 +1,7 @@
-/* $begin tinymain */
-/*
- * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the
- *     GET method to serve static and dynamic content.
- *
- * Updated 11/2019 droh
- *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
- */
 #include "csapp.h"
 
-void doit(int fd);
-
-// 
-void read_requesthdrs(rio_t *rp);
-int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
-void get_filetype(char *filename, char *filetype);
-void serve_dynamic(int fd, char *filename, char *cgiargs);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
