7196721aaf7ba0c153fede19e63c87dd380f0496
SeungHuLee <gadlsh12081208@gmail.com>
2022-12-14 11:28:48 +0900

    Add concurrent echo server

--- echo_concurrent/echoserver.c
+++ echo_concurrent/echoserver.c
@@ -1,26 +1,51 @@
 #include "csapp.h"
 
-int main(int argc, char **argv) 
+void echo(int connfd)
 {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
 
-  /* Check command line args */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-
-  listenfd = Open_listenfd(argv[1]);
-  while (1) {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // line:netp:tiny:accept
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    //doit(connfd);   // line:netp:tiny:doit
-    //Close(connfd);  // line:netp:tiny:close
-  }
+    Rio_readinitb(&rio, connfd);
+    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+    {
+        printf("Server received %d bytes.\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
 }
+
+void *thread(void *vargp)
+{
+  int connfd = *((int *)vargp);
+
+  Pthread_detach(pthread_self());
+
+  Free(vargp);
+
+  echo(connfd);
+
+  Close(connfd);
+
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+    int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+    listenfd = Open_listenfd(argv[1]);
+    
+    while (1)
+    {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfdp = Malloc(sizeof(int));
+        *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        
+        Pthread_create(&tid, NULL, thread, connfdp);
+    }
+    exit(0);
+}
+
